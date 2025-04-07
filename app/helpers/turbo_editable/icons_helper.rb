8befd41087aeb33f1287b708d33ff6875041d069
Grigory <mail@grigor.io>
2025-04-07 16:07:25 +0300

    Tabler icons

--- app/helpers/turbo_editable/icons_helper.rb
+++ app/helpers/turbo_editable/icons_helper.rb
@@ -5,26 +5,31 @@ module TurboEditable
     def editable_check_icon
       return editable_icon("fa fa-check") if TurboEditable.configuration.icons_framework == :fa
       return editable_icon("bi bi-check-lg") if TurboEditable.configuration.icons_framework == :bi
+      return editable_icon("ti ti-check") if TurboEditable.configuration.icons_framework == :ti
     end
 
     def editable_save_icon
       return editable_icon("fa fa-check") if TurboEditable.configuration.icons_framework == :fa
       return editable_icon("bi bi-floppy") if TurboEditable.configuration.icons_framework == :bi
+      return editable_icon("ti ti-device-floppy") if TurboEditable.configuration.icons_framework == :ti
     end
 
     def editable_x_icon
       return editable_icon("fa fa-times") if TurboEditable.configuration.icons_framework == :fa
       return editable_icon("bi bi-x-lg") if TurboEditable.configuration.icons_framework == :bi
+      return editable_icon("ti ti-x") if TurboEditable.configuration.icons_framework == :ti
     end
 
     def editable_pen_icon
       return editable_icon("fa fa-pen text-black") if TurboEditable.configuration.icons_framework == :fa
       return editable_icon("bi bi-pen text-black") if TurboEditable.configuration.icons_framework == :bi
+      return editable_icon("ti ti-pencil text-black") if TurboEditable.configuration.icons_framework == :ti
     end
 
     def editable_trash_icon
       return editable_icon("fa fa-trash text-black") if TurboEditable.configuration.icons_framework == :fa
       return editable_icon("bi bi-trash text-black") if TurboEditable.configuration.icons_framework == :bi
+      return editable_icon("ti ti-trash text-black") if TurboEditable.configuration.icons_framework == :ti
     end
 
     def editable_icon c
